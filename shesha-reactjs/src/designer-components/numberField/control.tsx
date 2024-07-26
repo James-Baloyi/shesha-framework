dc9e674a123aff6482b46392730d9f2277fe8c3b
James Baloyi <james_baloyi@yahoo.com>
2024-07-26 14:04:54 +0200

    number field additional properties

--- shesha-reactjs/src/designer-components/numberField/control.tsx
+++ shesha-reactjs/src/designer-components/numberField/control.tsx
@@ -1,9 +1,9 @@
 import { InputNumber, InputNumberProps, message } from 'antd';
 import moment from 'moment';
-import React, { FC } from 'react';
+import React, { CSSProperties, FC } from 'react';
 import { customInputNumberEventHandler } from '@/components/formDesigner/components/utils';
 import { useForm, useGlobalState, useSheshaApplication } from '@/providers';
-import { getStyle } from '@/providers/form/utils';
+import { getStyle, pickStyleFromModel } from '@/providers/form/utils';
 import { axiosHttp } from '@/utils/fetchers';
 import { INumberFieldComponentProps } from './interfaces';
 import { getFormApi } from '@/providers/form/formApi';
@@ -33,6 +33,22 @@ const NumberFieldControl: FC<IProps> = ({ disabled, model, onChange, value }) =>
 
   const style = model.style;
 
+  const styling = JSON.parse(model.stylingBox || '{}');
+  const stylingBoxAsCSS = pickStyleFromModel(styling);
+
+  const additionalStyles: CSSProperties = {
+    height: `${Number(model?.height) ? model?.height + 'px' : model?.height}`,
+    width: `${Number(model?.width) ? model?.width + 'px' : model?.width}`,
+    borderWidth: model?.hideBorder ? 0 : model?.borderSize,
+    borderRadius: model?.borderRadius,
+    borderStyle: model?.borderType,
+    borderColor: model?.borderColor,
+    backgroundColor: model?.backgroundColor,
+    fontSize: model?.fontSize,
+    ...stylingBoxAsCSS,
+  };
+
+
   const inputProps: InputNumberProps = {
     className: 'sha-number-field',
     disabled: disabled,
@@ -41,7 +57,7 @@ const NumberFieldControl: FC<IProps> = ({ disabled, model, onChange, value }) =>
     max: model?.max,
     placeholder: model?.placeholder,
     size: model?.size,
-    style: style ? getStyle(style, form.formData, globalState) : { width: '100%' },
+    style: style ? {...getStyle(style, form.formData, globalState), ...additionalStyles} : { ...additionalStyles, ...{ width: additionalStyles.width ?  additionalStyles?.width : '100%'}},
     step: model?.highPrecision ? model?.stepNumeric : model?.stepNumeric,
     ...customInputNumberEventHandler(eventProps, onChange),
     defaultValue: model?.defaultValue,
--- shesha-reactjs/src/designer-components/numberField/interfaces.ts
+++ shesha-reactjs/src/designer-components/numberField/interfaces.ts
@@ -8,4 +8,13 @@ export interface INumberFieldComponentProps extends IConfigurableFormComponent {
   stepNumeric?: number;
   stepString?: string;
   placeholder?: string;
+  borderSize?: number;
+  borderRadius?: number;
+  borderType?: string;
+  borderColor?: string;
+  fontSize?: string;
+  stylingBox?: string;
+  height?: string;
+  width?: string;
+  backgroundColor?: string;
 }
